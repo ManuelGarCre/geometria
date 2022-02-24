dbf90f65d45b401aa9dae1556f191f74110e6f72
Manuel García Cremades <mgarcia.severodam@hotmail.com>
2022-02-24 20:02:22 +0100

    Docuentar es de sabios, aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_MGC"},{"p":"<Unnamed>","l":"FiguraGeometrica_MGC"},{"p":"<Unnamed>","l":"Rectangulo_MGC"},{"p":"<Unnamed>","l":"Triangulo_MGC"}];updateSearchResults();
